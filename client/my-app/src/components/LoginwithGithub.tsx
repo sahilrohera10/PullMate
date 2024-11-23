2a7ef0f44506e2c1dd904eb7f23e337916498428
Sahil <sahilrohera10@gmail.com>
2024-11-23 18:35:43 +0530

    FEAT: created hero section of the landing page with responsive design and customised the github auth button

--- client/my-app/src/components/LoginwithGithub.tsx
+++ client/my-app/src/components/LoginwithGithub.tsx
@@ -2,14 +2,21 @@
 
 import React from "react";
 
-const LoginWithGitHub: React.FC = () => {
+const LoginWithGitHub = ({ title }: { title: string }) => {
   const loginWithGitHub = () => {
     const clientID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
     const baseURL = process.env.NEXT_PUBLIC_GITHUB_BASE_URL;
     const redirectURI = `${window.location.origin}/loginCallback`;
     window.location.href = `${baseURL}/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=repo,write:repo_hook`;
   };
-  return <button onClick={loginWithGitHub}>Login with GitHub</button>;
+  return (
+    <button
+      className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-xl text-lg"
+      onClick={loginWithGitHub}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default LoginWithGitHub;

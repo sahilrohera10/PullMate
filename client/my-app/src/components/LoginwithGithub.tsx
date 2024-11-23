abde08583ad6b434be47dc68cc066354afd32ff9
Sahil <sahilrohera10@gmail.com>
2024-11-23 21:07:59 +0530

    FIX: preventing un-neccesary hit to github url for auth and to our server also if the user is already authorized

--- client/my-app/src/components/LoginwithGithub.tsx
+++ client/my-app/src/components/LoginwithGithub.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const LoginWithGitHub = ({ title }: { title: string }) => {
+  const router = useRouter();
+
   const loginWithGitHub = () => {
-    const clientID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
-    const baseURL = process.env.NEXT_PUBLIC_GITHUB_BASE_URL;
-    const redirectURI = `${window.location.origin}/loginCallback`;
-    window.location.href = `${baseURL}/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=repo,write:repo_hook`;
+    const access_token = localStorage.getItem("access_token");
+    if (access_token) {
+      console.log("already loggedin access_token ->", access_token);
+      router.push("/loginCallback");
+    } else {
+      const clientID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
+      const baseURL = process.env.NEXT_PUBLIC_GITHUB_BASE_URL;
+      const redirectURI = `${window.location.origin}/loginCallback`;
+      window.location.href = `${baseURL}/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=repo,write:repo_hook`;
+    }
   };
   return (
     <button

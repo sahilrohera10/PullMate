cd701e0a81cf55764af882166b3871007badd2b1
bhawna <bhawnajain012003@gmail.com>
2024-11-27 22:02:31 +0530

    removed validation for user_id

--- server/src/controllers/workflow.controller.ts
+++ server/src/controllers/workflow.controller.ts
@@ -1,18 +1,9 @@
 import Joi from "joi";
 import { get_user_workflows } from "../services/workflow";
 import { Request, Response } from "express";
-import { userIdSchema } from "../validations/workflow.validations";
 
 export async function get_workflows(req: Request, res: Response): Promise<any> {
   const { user_id } = req.params;
-  const { error } = userIdSchema.validate(user_id);
-
-  if (error) {
-    return res.status(400).json({
-      message: "Validation error",
-      error: error.details.map((err) => err.message),
-    });
-  }
   try {
     const workflows = await get_user_workflows(user_id); //[@TODO] Add interface for workflows
 
--- server/src/validations/workflow.validations.ts
+++
@@ -1,3 +0,0 @@
-import Joi from "joi";
-
-export const userIdSchema = Joi.string().required().label("User ID");

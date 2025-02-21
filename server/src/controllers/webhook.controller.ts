1e2e83c2052117fbb242cf67d21df89767967a89
Sahil <sahilrohera10@gmail.com>
2025-02-22 03:49:35 +0530

    Fix space limit issue

--- server/src/controllers/webhook.controller.ts
+++ server/src/controllers/webhook.controller.ts
@@ -20,6 +20,12 @@ export async function handle_pr_webhook(
     ) {
       Logger.info("Webhook received");
       console.log("payload", payload);
+
+      const diff_url = payload.pull_request.diff_url;
+      // fetch the diff content
+      const diff_content = await fetch(diff_url);
+      const diff = await diff_content.text();
+      console.log("diff", diff);
       // Add a job of type 'email' to the 'mailer' queue
       // await payloadProcessingQueue.add("payload", payload);
       console.log("payload added into the queue");
